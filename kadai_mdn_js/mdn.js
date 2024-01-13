0fc332f402951c2935bde05c63904d478fe177b1
EmingoFramingo <152578769+EmingoFramingo@users.noreply.github.com>
2024-01-13 10:56:51 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
-const today =()=>{
-    const currentDate= Date()
-    console.log(currentDate)
+const today = () => {
+    const currentDate = new Date();
+    console.log(currentDate.getFullYear()+'年'+(currentDate.getMonth()+1)+'月',currentDate.getDate()+'日');
 }
 
-today()
+today();
